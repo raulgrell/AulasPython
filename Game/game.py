b7c1fbd73dbbc092ce1db593878c012477ee5233
raulgrell <raulgrell@gmail.com>
2019-04-09 14:37:08 +0100

    Refactor code

--- Game/game.py
+++ Game/game.py
@@ -8,16 +8,21 @@ from math import sqrt
 WIDTH = 600
 HEIGHT = 400
 
-BGCOLOR = (63, 255, 38)
-BALLCOLOR = (0, 0, 0)
 
-def random_colour():
+def get_random_colour():
     r = randint(0, 255)
     g = randint(0, 255)
     b = randint(0, 255)
     return (r, g, b)
 
+
+def dist(p1, p2):
+    return sqrt((p1[0] - p2[0])**2 + (p1[1]-p2[1])**2)
+
+
 class Ball(object):
+    color = (0, 0, 0)
+
     def __init__(self, start_x, start_y, radius):
         self.posX = start_x
         self.posY = start_y
@@ -26,61 +31,53 @@ class Ball(object):
         self.radius = radius
 
     def draw(self, surface):
-        pygame.draw.circle(
-            surface,
-            BALLCOLOR,
-            (int(self.posX), int(self.posY)),
-            int(self.radius))
+        pos = (int(self.posX), int(self.posY))
+        pygame.draw.circle(surface, Ball.color, pos, int(self.radius))
 
     def update(self):
         self.velY += 0.00001
         self.posY += self.velY
 
-ball = Ball(WIDTH / 2, HEIGHT / 2, 30)
 
 class Game(object):
-    def __init__(self, surf, score = 0):
+    background_color = (63, 255, 38)
+
+    def __init__(self, surf, score=0):
         self.surface = surf
-        ### Variavel não tem ()
         self.score = score
-        ### Uma função tem ()
+        self.ball = Ball(WIDTH / 2, HEIGHT / 2, 30)
         self.reset()
 
     def reset(self):
-        ball.posX = WIDTH / 2
-        ball.posY = 0
-        
-        ball.velX = 0
-        ball.velY = 0
-        
+        self.ball.posX = WIDTH / 2
+        self.ball.posY = 0
+
+        self.ball.velX = 0
+        self.ball.velY = 0
+
         self.score = 0
 
-    def dist(self, p1, p2):
-        return sqrt((p1[0] - p2[0])**2 + (p1[1]-p2[1])**2)
-
     def draw(self):
-        self.surface.fill(BGCOLOR)
-        ball.draw(self.surface)
+        self.surface.fill(Game.background_color)
+        self.ball.draw(self.surface)
 
     def update(self):
-        ball.update()
+        self.ball.update()
 
-        if ball.posY > HEIGHT:
-            self.reset() 
+        if self.ball.posY > HEIGHT:
+            self.reset()
 
     def handle_click(self, ev):
         if ev.button == 1:
-            posBall = (ball.posX, ball.posY)
+            posBall = (self.ball.posX, self.ball.posY)
             posMouse = ev.pos
-            d = self.dist(posBall, posMouse)
-            if d <= ball.radius:
-                ball.velY = -0.05
+            d = dist(posBall, posMouse)
+            if d <= self.ball.radius:
+                self.ball.velY = -0.05
             else:
                 pass
         elif ev.button == 3:
             pass
-            
-
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 game = Game(screen)
@@ -98,4 +95,5 @@ while True:
 
     game.update()
     game.draw()
+
     pygame.display.update()
